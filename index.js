3ebe8862cb61e76cbac190c89b8ae675b54701f5
Jason Pitts <juniorjr083@gmail.com>
2018-09-17 21:25:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function nameToAttributes(drivers){
   }
   console.log(arr);
   return arr.map(function(ele){
-    obj[first_name] = ele[0];
-    obj[last_name] = ele[1];
+    obj[firstName] = ele[0];
+    obj[lastName] = ele[1];
     return obj;
     
   });
