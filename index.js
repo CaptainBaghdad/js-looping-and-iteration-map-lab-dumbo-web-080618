9f494c4d8a91132df45a5dac9854f4b9760118cd
Jason Pitts <juniorjr083@gmail.com>
2018-09-17 21:45:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nameToAttributes(drivers){
   //console.log(arr);
   return arr.map(function(ele){
     
-   {'firstName': ele.split(' ')[0], 'lastName': ele.split(' ')[1]};
+   console.log({'firstName': ele.split(' ')[0], 'lastName': ele.split(' ')[1]});
    
     
   });
